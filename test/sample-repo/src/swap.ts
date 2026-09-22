8788d559e4988ce85c4fba86287f0fe05ca926a6
blinkenstephanie <121748498+blinkenstephanie@users.noreply.github.com>
2026-09-21 19:06:05 -0700

    Initial apex-pumpswap-doctor 0.1.0 (CLI + Action)

---
+++ test/sample-repo/src/swap.ts
@@ -0,0 +1,14 @@
+import { something } from "@pump-fun/pump-swap-sdk";
+const PUMP_SWAP_PROGRAM_ID = "6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P";
+export async function buy() {
+  return {
+    accounts: {
+      pool: null,
+      user: null,
+      user_base_token_account: null,
+      user_quote_token_account: null,
+    },
+  };
+}
+void something;
+void PUMP_SWAP_PROGRAM_ID;
